8a4e728efcb4146e99ea3738b7f576dbf8d5140c
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2026-05-06 09:52:11 +0200

    .

--- JaebeMusicStudio3.Front/TimelineGui/TimelineItemGui.xaml.cs
+++ JaebeMusicStudio3.Front/TimelineGui/TimelineItemGui.xaml.cs
@@ -11,27 +11,53 @@ public partial class TimelineItemGui : UserControl
     public TimelineItemGui(ITimelineItem item, double secondsPerPixel)
     {
         InitializeComponent();
-        // if (item is RecordedSound itemC)
-        // {
-        //     var line = new Polyline();
-        //     line.Stroke = System.Windows.Media.Brushes.Green;
-        //     MainGrid.Children.Add(line);
-        //     var halfHeight = 50;
-        //     if (itemC.Samples != null)
-        //     {
-        //         var widthOfSample = 1.0 / itemC.WaveFormat.SampleRate / secondsPerPixel;
-        //         var i = 0;
-        //         foreach (var sample in itemC.Samples)
-        //         {
-        //             line.Points.Add(
-        //                 new Point(
-        //                     i * widthOfSample,
-        //                     sample * halfHeight + halfHeight
-        //                 )
-        //             );
-        //             i++;
-        //         }
-        //     }
-        // }
+        if (item is RecordedSound itemC)
+        {
+            //     var line = new Polyline();
+            //     line.Stroke = System.Windows.Media.Brushes.Green;
+            //     MainGrid.Children.Add(line);
+            //     var halfHeight = 50;
+            //     if (itemC.Samples != null)
+            //     {
+            //         var widthOfSample = 1.0 / itemC.WaveFormat.SampleRate / secondsPerPixel;
+            //         var i = 0;
+            //         foreach (var sample in itemC.Samples)
+            //         {
+            //             line.Points.Add(
+            //                 new Point(
+            //                     i * widthOfSample,
+            //                     sample * halfHeight + halfHeight
+            //                 )
+            //             );
+            //             i++;
+            //         }
+            //     }
+        }
+        else if (item is NoteLine itemN)
+        {
+            if (itemN.Notes.Any())
+            {
+                var grid = new Grid();
+                grid.Background = System.Windows.Media.Brushes.LightGray;
+                var minPitch = Math.Log2(itemN.Notes.Min(x => x.Pitch)) * 12;
+                var maxPitch = Math.Log2(itemN.Notes.Max(x => x.Pitch)) * 12;
+                foreach (var n in itemN.Notes)
+                {
+                    var pitch = Math.Log2(n.Pitch) * 12;
+                    var rect = new Rectangle();
+                    rect.VerticalAlignment = VerticalAlignment.Top;
+                    rect.HorizontalAlignment = HorizontalAlignment.Left;
+                    rect.Margin = new Thickness(n.Start / itemN.Tempo * 60 / secondsPerPixel,
+                        (pitch - minPitch) / (maxPitch - minPitch + 1) * 50, 0, 0);
+                    rect.Width = n.Length / itemN.Tempo * 60 / secondsPerPixel;
+                    rect.Height = 1 / (maxPitch - minPitch + 1) * 50;
+                    // rect.Fill = System.Windows.Media.Brushes.Green;
+                    rect.Stroke = System.Windows.Media.Brushes.Black;
+                    grid.Children.Add(rect);
+                }
+
+                MainGrid.Children.Add(grid);
+            }
+        }
     }
 }
