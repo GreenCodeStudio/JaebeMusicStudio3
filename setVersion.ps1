b87ba4317b8625beed20db9637173d4678f247db
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2026-05-17 17:54:50 +0200

    .

--- setVersion.ps1
+++ setVersion.ps1
@@ -1,17 +1,19 @@
-﻿[xml]$installerCsproj= Get-Content .\JaebeMusicStudio3.Installer\JaebeMusicStudio3.Installer.csproj;
+﻿$version=$args[0] -replace 'refs/tags/v', '';
+
+[xml]$installerCsproj= Get-Content .\JaebeMusicStudio3.Installer\JaebeMusicStudio3.Installer.csproj;
 $installerCsprojVersion=$installerCsproj.CreateElement('AssemblyVersion');
-$installerCsprojVersion.AppendChild($installerCsproj.CreateTextNode($args[0]));
+$installerCsprojVersion.AppendChild($installerCsproj.CreateTextNode(version));
 $installerCsproj.GetElementsByTagName('PropertyGroup')[0].AppendChild($installerCsprojVersion);
 $installerCsproj.Save('.\JaebeMusicStudio3.Installer\JaebeMusicStudio3.Installer.csproj');
 
 [xml]$frontendCsproj= Get-Content .\JaebeMusicStudio3.Front\JaebeMusicStudio3.Front.csproj;
 $frontendCsprojVersion=$frontendCsproj.CreateElement('AssemblyVersion');
-$frontendCsprojVersion.AppendChild($frontendCsproj.CreateTextNode($args[0]));
+$frontendCsprojVersion.AppendChild($frontendCsproj.CreateTextNode(version));
 $frontendCsproj.GetElementsByTagName('PropertyGroup')[0].AppendChild($frontendCsprojVersion);
 $frontendCsproj.Save('.\JaebeMusicStudio3.Front\JaebeMusicStudio3.Front.csproj');
 
 
 
 $versionCs=Get-Content .\JaebeMusicStudio3.Core\Version.cs ;
-$versionCs=$versionCs -replace 'public static readonly string VERSION = ".*";', "public static readonly string VERSION = `"$args`";";
+$versionCs=$versionCs -replace 'public static readonly string VERSION = ".*";', "public static readonly string VERSION = `"$version`";";
 Set-Content -Path .\JaebeMusicStudio3.Core\Version.cs -Value $versionCs
