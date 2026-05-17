3e3bad6f1afd0bacc84ac4e3a180175b19b041c2
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2026-05-17 15:20:37 +0200

    .

--- JaebeMusicStudio3.Installer/Form1.Designer.cs
+++ JaebeMusicStudio3.Installer/Form1.Designer.cs
@@ -32,15 +32,21 @@
             button1 = new Button();
             Path = new TextBox();
             button2 = new Button();
+            splitContainer1 = new SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F);
             label1.Location = new Point(22, 9);
             label1.Name = "label1";
-            label1.Size = new Size(229, 25);
+            label1.Size = new Size(459, 48);
             label1.TabIndex = 0;
             label1.Text = "Jaebe Music Studio Installer";
             label1.TextAlign = ContentAlignment.MiddleCenter;
@@ -48,9 +54,10 @@
             // 
             // button1
             // 
-            button1.Location = new Point(457, 153);
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button1.Location = new Point(3, 0);
             button1.Name = "button1";
-            button1.Size = new Size(112, 34);
+            button1.Size = new Size(379, 362);
             button1.TabIndex = 2;
             button1.Text = "Run portable";
             button1.UseVisualStyleBackColor = true;
@@ -58,33 +65,57 @@
             // 
             // Path
             // 
-            Path.Location = new Point(22, 58);
+            Path.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            Path.Location = new Point(10, 16);
             Path.Name = "Path";
-            Path.Size = new Size(356, 31);
+            Path.Size = new Size(383, 31);
             Path.TabIndex = 3;
             // 
             // button2
             // 
-            button2.Location = new Point(60, 132);
+            button2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button2.Location = new Point(3, 325);
             button2.Name = "button2";
-            button2.Size = new Size(112, 34);
+            button2.Size = new Size(390, 34);
             button2.TabIndex = 4;
             button2.Text = "Install";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
+            // splitContainer1
+            // 
+            splitContainer1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            splitContainer1.Location = new Point(12, 60);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(Path);
+            splitContainer1.Panel1.Controls.Add(button2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(button1);
+            splitContainer1.Size = new Size(785, 362);
+            splitContainer1.SplitterDistance = 396;
+            splitContainer1.TabIndex = 5;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button2);
-            Controls.Add(Path);
-            Controls.Add(button1);
+            AutoScroll = true;
+            ClientSize = new Size(809, 434);
+            Controls.Add(splitContainer1);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -95,5 +126,6 @@
         private Button button1;
         private TextBox Path;
         private Button button2;
+        private SplitContainer splitContainer1;
     }
 }
